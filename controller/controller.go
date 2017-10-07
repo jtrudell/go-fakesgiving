d677dc55dd6fd5a452449f926b31bdb5575c2fcf
Jen <jtrudell@gmail.com>
2017-10-07 18:57:15 -0500

    add favicon

--- controller/controller.go
+++ controller/controller.go
@@ -15,5 +15,10 @@ func Init(templates map[string]*template.Template) {
 	indexController.registerRoutes()
 	signupController.signupTemplate = templates["signup.html"]
 	signupController.registerRoutes()
+	http.HandleFunc("/favicon.ico", handleFavicon)
 	http.Handle("/css/", http.FileServer(http.Dir("public")))
+}
+
+func handleFavicon(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "/public/favicon.ico")
 }
